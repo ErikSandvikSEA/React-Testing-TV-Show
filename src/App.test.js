30b33a9fe39f4ce7544aeceb0841f3dc22204714
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-18 12:31:49 -0700

    added testing functionality to the Episodes component

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,8 @@
 import React from 'react'
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from './App'
+
+test('renders App without errors', () => {
+     render(<App />)
+})
