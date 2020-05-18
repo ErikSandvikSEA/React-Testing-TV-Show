d7867af7eb3d8d892e1e8d0682a439b0ef24b553
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-05-18 14:47:38 -0700

    added testing functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import { fetchShow } from './api/fetchShow'
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -14,18 +14,28 @@ export default function App() {
   const [selectedSeason, setSelectedSeason] = useState("");
   const episodes = seasons[selectedSeason] || [];
 
+  // const fetchShow = () => {
+//     axios
+//     .get(
+//       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+//     )
+//     .then(res => {
+//       setShow(res.data);
+//       setSeasons(formatSeasons(res.data._embedded.episodes));
+//     });
+// };
+
+
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    fetchShow()
+      .then(res => {
+        console.log(res)
+      setShow(res.data)
+      setSeasons(formatSeasons(res.data._embedded.episodes))
+    })
+      .catch(err => {
+        console.log(err)
+      })
   }, []);
 
   const handleSelect = e => {
@@ -46,8 +56,9 @@ export default function App() {
         onChange={handleSelect}
         value={selectedSeason || "Select a season"}
         placeholder="Select an option"
+        data-testid='dropdown'
       />
-      <Episodes episodes={episodes} />
+      <Episodes episodes={episodes} data-testid='episodes'  />
     </div>
   );
 }
